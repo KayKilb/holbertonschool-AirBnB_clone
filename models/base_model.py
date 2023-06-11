eff38c24cebea5a4eddb28999772f3afe095d298
Night8Guy <robert@lionsroad.com>
2023-06-11 11:26:32 -0500

    test files and engine

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,44 @@
+#!/usr/bin/python3
+"""
+File containing Base model class for AirBnB - The Console
+"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
-class BaseModel:
-    def __init__(self):
+class BaseModel():
+    """Base Model Class"""
+    def __init__(self, *args, **kwargs):
+        """Initialization of BaseModel class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+        else:
+            storage.new(self)
+            
 
-    def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """Saves and updates the date"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        """Dictionary representation of BaseModel"""
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
+
+    def __str__(self):
+        """String representation function"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
