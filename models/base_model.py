0b0de2a50bcb0af0e771f4a4a6d1bd943cb55cba
Night8Guy <robert@lionsroad.com>
2023-06-12 11:39:38 -0500

    Partner Work Coding together

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,45 @@
+#!/usr/bin/python3
+"""BaseModel that defines all common attributes/methods for other classes"""
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
+    """basemodel class"""
     def __init__(self, *args, **kwargs):
+        """initializes class"""
         if kwargs:
             for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
+        """string representation of base model"""
         return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__
-        )
+            type(self).__name__, self.id, self.__dict__)
+
+    def __repr__(self):
+        """returns string function"""
+        return self.__str__()
 
     def save(self):
+        """saves the class"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        """gives a dict of object"""
+        copy = dict(self.__dict__)
+        copy['__class__'] = str(self.__class__.__name__)
+        copy['created_at'] = self.created_at.isoformat()
+        copy['updated_at'] = self.updated_at.isoformat()
+        return copy
+    
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,34 +1,43 @@
-import json
+"""This moduleestablishes the FileStorage class for seriealization and decerialization"""
 
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class FileStorage:
+    """This class handles serialization and decserialization of JSON file objects"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """returns dictionary of objects"""
         return self.__objects
 
     def new(self, obj):
+        """adds new object to __objects"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        obj_dict = {}
-        for key, obj in self.__objects.items():
-            obj_dict[key] = obj.to_dict()
-        with open(self.__file_path, 'w') as file:
-            json.dump(obj_dict, file)
+        """saves objects to json file"""
+        jsonData = {}
+        for key, value in self.__objects.items():
+            jsonData[key] = value.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(jsonData, f)
 
     def reload(self):
+        """reloads"""
         try:
-            with open(self.__file_path, 'r') as file:
-                obj_dict = json.load(file)
-
-            from models.base_model import BaseModel
-            for key, value in obj_dict.items():
-                cls_name, obj_id = key.split('.')
-                cls = models.classes[cls_name]
-                obj = cls(**value)
-                self.__objects[key] = obj
+            with open(self.__file_path, 'r') as f:
+                data = json.load(f)
+                for key, obj in data.items():
+                    newObj = eval(obj['__class__'])(**obj)
+                    self.__objects[key] = newObj
         except FileNotFoundError:
             pass
