7123a3a1285399ea937f21d17fc472fb2b958c75
Night8Guy <robert@lionsroad.com>
2023-06-12 10:00:57 -0500

    retrying task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,31 @@
-#!/usr/bin/python3
-"""
-File containing Base model class for AirBnB - The Console
-"""
 import uuid
 from datetime import datetime
-from models import storage
 
 
-class BaseModel():
-    """Base Model Class"""
+class BaseModel:
     def __init__(self, *args, **kwargs):
-        """Initialization of BaseModel class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        if kwargs is not None:
+        if kwargs:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                setattr(self, key, value)
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
         else:
-            storage.new(self)
-            
-
-
-    def save(self):
-        """Saves and updates the date"""
-        self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
-
-    def to_dict(self):
-        """Dictionary representation of BaseModel"""
-        data = self.__dict__.copy()
-        data['__class__'] = self.__class__.__name__
-        data['created_at'] = self.created_at.isoformat()
-        data['updated_at'] = self.updated_at.isoformat()
-        return data
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        """String representation function"""
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
